af19cfccabc1f706b2d717465ac8a56e1c761628
Carlos Vasquez <carlos.vasquez@sellside.cl>
2023-01-10 16:06:08 -0300

    Actualizacion TextFields

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:coffeemondo/iconos.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -7,10 +8,9 @@ class Login extends StatefulWidget {
 
 class LoginApp extends State<Login> {
   bool _obscureText = true;
-
+  bool obs = true;
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xffffebdcac),
       appBar: AppBarcustom(),
       body: SingleChildScrollView(
         child: Column(children: [
@@ -24,48 +24,51 @@ class LoginApp extends State<Login> {
                 ),
               )),
           Padding(
-              padding: EdgeInsets.all(20),
+              padding: EdgeInsets.only(left: 50, top: 50, right: 40),
               child: TextField(
-                decoration: InputDecoration(
-                    border: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.red,
-                      ),
-                    ),
-                    labelText: 'Usuario',
-                    hintText: 'Escriba su nombre de Usuario'),
-              )),
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.account_circle_outlined),
+                      hintText: 'Correo electronico'))),
           Padding(
-            padding: EdgeInsets.all(20),
-            child: TextField(
-              decoration: InputDecoration(
-                suffixIcon: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      _obscureText = !_obscureText;
-                    });
-                  },
-                  child: Icon(
-                      _obscureText ? Icons.visibility : Icons.visibility_off),
-                ),
-                border: UnderlineInputBorder(),
-                labelText: 'Contraseña',
-                hintText: 'Escriba su Contraseña',
+              padding: EdgeInsets.only(left: 50, top: 10, right: 40),
+              child: TextField(
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.remove_red_eye),
+                        onPressed: () {
+                          setState(() {
+                            obs == true ? obs = true : obs = false;
+                          })
+                        },
+                        ),
+                      hintText: 'Contraseña'))),
+          Padding(
+            padding: EdgeInsets.only(left: 20, top: 50, right: 10),
+            child: Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => Login()));
+                },
+                child: Text('Entrar'),
               ),
-              obscureText: _obscureText,
             ),
           ),
           Padding(
             padding: EdgeInsets.only(left: 20, top: 50, right: 10),
-            child: CustomButton1(),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 20, top: 20, right: 10),
-            child: CustomButton2(),
+            child: Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => Login()));
+                },
+                child: Text('Soy nuevo'),
+              ),
+            ),
           )
         ]),
       ),
-      bottomNavigationBar: CustomBottomBar(),
     );
   }
 }
@@ -75,26 +78,29 @@ class AppBarcustom extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width,
-      height: 88.0,
+      height: 100,
       child: ClipPath(
         clipper: BackgroundAppBar(),
         child: Container(
           width: MediaQuery.of(context).size.width,
           height: 100,
           decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 207, 111, 55),
-              Color.fromARGB(255, 207, 111, 55)
-            ]),
+            gradient: LinearGradient(
+                colors: [Color.fromARGB(255, 207, 111, 55), Color(0xFFF6EFE9)]),
           ),
-          child: Center(
-            child: Text(
-              'Iniciar Sesion',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 25,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () {},
               ),
-            ),
+              Text('Ingresar'),
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {},
+              )
+            ],
           ),
         ),
       ),
@@ -102,7 +108,7 @@ class AppBarcustom extends StatelessWidget implements PreferredSizeWidget {
   }
 
   @override
-  Size get preferredSize => Size.fromHeight(88.0);
+  Size get preferredSize => Size.fromHeight(100);
 }
 
 class BackgroundAppBar extends CustomClipper<Path> {
@@ -111,161 +117,11 @@ class BackgroundAppBar extends CustomClipper<Path> {
     var path = Path();
     path.lineTo(0.0, size.height);
 
-    path.lineTo(size.width, size.height - 20);
+    path.lineTo(size.width, size.height - 15);
     path.lineTo(size.width, 0.0);
     return path;
   }
 
   @override
   bool shouldReclip(BackgroundAppBar oldClipper) => oldClipper != this;
-}
-
-class CustomBottomBar extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 80.0,
-      width: double.infinity,
-      child: ClipPath(
-        clipper: BackgroundBottomBar(),
-        child: Container(
-          color: Color.fromARGB(255, 207, 111, 55),
-        ),
-      ),
-    );
-  }
-}
-
-class BackgroundBottomBar extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    final path = Path();
-    path.lineTo(0.0, size.height);
-    path.lineTo(size.width, size.height);
-    path.lineTo(size.width, 0);
-    path.lineTo(size.width - 640, size.height);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-    return false;
-  }
-}
-
-class CustomButton1 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Container(
-        width: 250,
-        height: 50,
-        child: CustomPaint(
-          painter: BackgroundButton1(),
-          child: InkWell(
-            onTap: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (_) => Login()));
-            },
-            child: Center(
-              child: Text(
-                'Entrar',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class BackgroundButton1 extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    Color color1 = Color.fromARGB(255, 97, 2, 185);
-    Color color2 = Color.fromARGB(255, 43, 0, 83);
-
-    final LinearGradient gradient = LinearGradient(
-        begin: Alignment.topCenter,
-        end: Alignment.bottomCenter,
-        colors: [color1, color2]);
-
-    final paint = Paint()
-      ..shader =
-          gradient.createShader(Rect.fromLTWH(0, 0, size.width, size.height));
-
-    var path = Path();
-    path.lineTo(size.width - 240, size.height - 10);
-    path.lineTo(size.width - 10, size.height - 3);
-    path.lineTo(size.width, size.height - 52);
-
-    path.close();
-
-    canvas.drawPath(path, paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
-  }
-}
-
-class CustomButton2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Container(
-        width: 250,
-        height: 50,
-        child: CustomPaint(
-          painter: BackgroundButton2(),
-          child: InkWell(
-            onTap: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (_) => Login()));
-            },
-            child: Center(
-              child: Text(
-                'Soy Nuevo',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 97, 2, 185),
-                  fontSize: 20,
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class BackgroundButton2 extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    Color color1 = Color.fromARGB(255, 97, 2, 185);
-    final paint = Paint()
-      ..color = color1
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 2;
-
-    var path = Path();
-    path.lineTo(size.width - 240, size.height - 10);
-    path.lineTo(size.width - 10, size.height - 3);
-    path.lineTo(size.width, size.height - 52);
-
-    path.close();
-
-    canvas.drawPath(path, paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
-  }
 }
