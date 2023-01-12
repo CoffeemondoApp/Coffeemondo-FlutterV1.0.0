08541e3f72a65b030a6b3920d0aa11918e4c10f6
Jose Miguel Alejandro Sepulveda Pino <josealejandrosepulvedapino@gmail.com>
2023-01-12 15:34:46 -0300

    DebugBanner Off

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
         title: 'login app',
         home: Scaffold(
           backgroundColor: Color(0xffffebdcac),
