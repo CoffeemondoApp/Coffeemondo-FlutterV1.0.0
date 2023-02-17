3cbffb7a74472de15651bca6a1c0698d05d4567c
Jose Miguel Alejandro Sepulveda Pino <josealejandrosepulvedapino@gmail.com>
2023-02-17 16:13:51 -0300

    Notificaciones PUSH, prueba
    prueba de funcionamiento

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:coffeemondo/pantallas/FirebaseMessaging.dart';
 import 'package:coffeemondo/widget_tree.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -5,6 +6,7 @@ import 'package:flutter/material.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
    await Firebase.initializeApp();
+   await FirebaseMessagingService().setupFirebase();
   runApp(const MyApp());
 }
 
